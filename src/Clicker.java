a692e8180ed2db6fca4ab7979ae5b2d38b9e05a7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-14 17:45:44 -0700

    hi

---
+++ src/Clicker.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Clicker implements ActionListener {
+	JFrame window = new JFrame();
+	JPanel panel = new JPanel();
+	JButton incButton = new JButton("Inc");
+	JButton discButton = new JButton("Disc");
+	public static void main(String[] args) {
+	Clicker a = new Clicker();
+	a.buildWindow();
+	}
+	void buildWindow() {
+		window.add(panel);
+		panel.add(incButton);
+		panel.add(discButton);
+		window.setVisible(true);
+		window.pack();
+		incButton.addActionListener(this);
+		discButton.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Boi");
+	}
+}
